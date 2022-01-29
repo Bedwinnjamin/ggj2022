7b471a9fbbcfd69cca2607f2321d4b3f4f8d13ed
Jeb Bishop <jeb@jeb.zone>
2022-01-29 17:09:02 -0500

    Work on character controller and tictactoe game logic

---
+++ Duality/Assets/LetterManager.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LetterManager : MonoBehaviour
+{
+    public TextMesh textMesh;
+    public Color LetterColor;
+
+    private static string[] possibleLetters = {"X", "O"};
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        textMesh.color = LetterColor;
+        textMesh.text = possibleLetters[Random.Range(0,2)];
+    }
+
+    void SwitchLetter()
+    {
+        textMesh.text = (textMesh.text == "X") ? "O" : "X";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            SwitchLetter();
+        }
+    }
+
+    public void OfferClaim()
+    {
+        Debug.Log("Can claim this spot!");
+        textMesh.color = new Color()
+    }
+}
